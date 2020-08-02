bf63905ba93c6338c4cdf6fe872c324f74543f65
Alejgg <57809406+Alejgg@users.noreply.github.com>
2020-08-02 13:22:37 -0500

    Create Usuarios_Grupos.sh

---
+++ Usuarios_Grupos.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+clear -x
+
+echo -e "Sistema de administracion de usuarios y grupos \n¿Qué desea hacer?"
+
+#MENU IMPRIMIR
+datos (){
+    echo -e "1) Crear_Grupo\n2) Crear_Usuario\n3) Salir"
+}
+
+#CREAR GRUPO PREDETERMINADO
+Crear_Grupo1 (){
+    echo "Digite el nombre del grupo a crear"
+    read namegroup
+    if grep -q $namegroup /etc/group
+    then
+        echo -e "El grupo $namegroup ya existe \nVolviendo al menu anterior"
+        Opcion_Grupo
+    else
+        addgroup $namegroup
+        echo -e "Se ha creado un grupo con el nombre de $namegroup\nVolviendo al menu anterior"
+        Opcion_Grupo
+    fi
+}
+#CREAR GRUPO PERSONALIZADO
+Crear_Grupo2 (){
+    echo "Digite el nombre del grupo a crear"
+    read namegroup
+    echo "Digite el ID del grupo"
+    read ID
+    if grep -q $namegroup /etc/group
+    then
+        echo -e "El grupo $namegroup ya existe \nVolviendo al menu anterior"
+        Opcion_Grupo
+    else
+        echo "Desea introducir una contrasena y/n?"
+        read respuesta
+        if [[ $respuesta = Y || $respuesta = y ]]; then
+            echo "Contrasena:"
+            read pass
+            groupadd -g $ID -p "$pass" $namegroup
+            echo -e "Se ha creado un grupo con el nombre de $namegroup con el ID $ID
+                        y contrasena $pass\nVolviendo al menu anterior"
+            Opcion_Grupo
+            elif [[ $respuesta = n || $respuesta = N ]]; then
+            groupadd -g $ID $namegroup
+            echo -e "Se ha creado un grupo con el nombre de $namegroup e ID $ID\nVolviendo al menu anterior"
+            Opcion_Grupo
+        fi
+        
+    fi
+}
+#OPCION GRUPO
+Opcion_Grupo (){
+    echo -e "Ha seleccionado el añadir un nuevo grupo"
+    menu="Grupo_Predeterminado Grupo_Personalizado Atras"
+    select menu in $menu
+    do
+        if [ $menu = "Grupo_Predeterminado" ]; then
+            echo "Creando grupo predeterminado"
+            Crear_Grupo1
+            elif [ $menu = "Grupo_Personalizado" ]; then
+            echo "Creando grupo con id personalizado"
+            Crear_Grupo2
+            elif [ $menu = "Atras" ]; then
+            echo "Menu anterior"
+            menu
+        else
+            echo "Opcion no valida"
+        fi
+        
+    done
+}
+
+#MENU PRINCIPAL
+menu (){
+    menu="Crear_Grupo Crear_Usuario Salir"
+    select menu in $menu
+    do
+        if [ $menu = "Crear_Grupo" ]; then
+            Opcion_Grupo
+            elif [ $menu = "Crear_Usuario" ]; then
+            echo "hola2"
+            elif [ $menu = "Salir" ]; then
+            echo "Fin de la sesion"
+            exit
+        else
+            echo "Opcion no valida"
+            datos
+        fi
+        
+    done
+}
+
+menu
