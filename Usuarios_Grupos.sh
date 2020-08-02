58f35d059879b31677130ed1f23855831fc6b4ac
Alejgg <57809406+Alejgg@users.noreply.github.com>
2020-08-02 15:24:00 -0500

    Update Usuarios_Grupos.sh

--- Usuarios_Grupos.sh
+++ Usuarios_Grupos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-clear -x
+datos (){
+        echo -e "1) Crear_Grupo\n2) Crear_Usuario\n3) Listar_Usuarios\n4) Listar_Grupos\n5) Salir"
 
+}
 #CREAR GRUPO PREDETERMINADO
 Crear_Grupo1 (){
     echo "Digite el nombre del grupo a crear"
@@ -42,10 +44,23 @@ Crear_Grupo2 (){
         
     fi
 }
+#ANADIR GRUPO
+Anadir_Grupo (){
+    clear -x
+    echo "Lista de grupos existentes:"
+    cat /etc/group | cut -d: -f1,3 | nl
+    cho "Ingrese nombre o id del grupo"
+    read group
+    echo "Ingrese el usuario a agregar en este grupo"
+    read user
+    usermod -a -G $group $user
+    echo "Usuario $user agregado al grupo $group"
+
+}
 #OPCION GRUPO
 Opcion_Grupo (){
     echo -e "Ha seleccionado el añadir un nuevo grupo"
-    menu="Grupo_Predeterminado Grupo_Personalizado Atras"
+    menu="Grupo_Predeterminado Grupo_Personalizado Anadir_Usuario_a_un_Grupo Atras"
     select menu in $menu
     do
         if [ $menu = "Grupo_Predeterminado" ]; then
@@ -54,6 +69,8 @@ Opcion_Grupo (){
             elif [ $menu = "Grupo_Personalizado" ]; then
             echo "Creando grupo con id personalizado"
             Crear_Grupo2
+            elif [ $menu = "Anadir_Usuario_a_un_Grupo" ]; then
+            Anadir_Grupo
             elif [ $menu = "Atras" ]; then
             echo "Menu anterior"
             menu
@@ -78,14 +95,62 @@ Crear_Usuario1 (){
         Opcion_Usuario
     fi
 }
+caso1 (){
+    echo "Creando Directorio Home, Shell, Comentario, UID y GUI"
+    echo "El usuario a crear es $nameuser"
+    echo "Insertar el Directorio Home:"
+    read home
+    echo "Insertar el shell:"
+    read shell
+    echo "Insertar comentario: "
+    read comentario
+    echo "Insertar el user ID: "
+    read uID
+    while grep -q $uID /etc/passwd
+    do
+        echo "ID de usuario repetido!! prueba con otro: "
+        read uID
+    done
+    echo "Desea crear un grupo con el mismo nombre del usuario $username Y/n?"
+    read respuesta
+    if [[ $respuesta = Y || $respuesta = y ]]; then
+        echo "Creando grupo $nameuser con ID $uID"
+        groupadd -g $uID $nameuser
+        elif [[ $respuesta = n || $respuesta = N ]]; then
+        echo -e "Grupo no creado"
+    fi
+    clear -x
+    echo "Lista ultimos 10 grupos existentes"
+    tail /etc/group | cut -d: -f1,3
+    echo "Insertar el group ID: "
+    read GID
+    echo "El usuario a crear quedaria de la siguiente manera:"
+    echo "$nameuser:x:$uID:$GID:$comentario:$home:$shell:"
+    echo "Desea continuar Y/n?"
+    read respuesta
+    if [[ $respuesta = Y || $respuesta = y ]]; then
+        echo "Creando usuario"
+        useradd -m -d $home -s $shell -c "$comentario" -u $UID -g $GID $nameuser
+        elif [[ $respuesta = n || $respuesta = N ]]; then
+        echo -e "Usuario no creado"
+    fi
+    echo "Desea poner una contrasena al usuario $nameuser creado Y/n?"
+    read respuesta
+    if [[ $respuesta = Y || $respuesta = y ]]; then
+        passwd $nameuser
+        elif [[ $respuesta = n || $respuesta = N ]]; then
+        echo -e "Usuario $username sin contrasena"
+    fi
+}
 Usuario (){
+    clear -x
     while [ "$opcion" != 0 ]
     do
         
         echo  "MENU"
         echo "----------"
-        echo "1) Crear un usuario con Directorio Home, Shell, Comentario, UID y GUI"
-        echo "2) Crear un usuario sin Directorio Home, sin Shell, sin Grupo y un Comentario"
+        echo "1) Crear Directorio Home, Shell, Comentario, UID y GUI para $nameuser"
+        echo "2) No Crear Directorio Home, sin Shell, sin Grupo y un Comentario para $nameuser"
         echo "0) Salir"
         echo "----------"
         
@@ -93,35 +158,13 @@ Usuario (){
         
         case $opcion in
             1)
-                echo "Crear un usuario con Directorio Home, Shell, Comentario, UID y GUI"
-                echo "El usuario actual es $nameuser"
-                echo "insertar el Directorio Home:"
-                read home
-                echo "insertar el shell:"
-                read shell
-                echo "insertar comentario: "
-                read comentario
-                echo "insertar el user ID: "
-                read UID
-                echo "insertar el group ID: "
-                read GID
-                echo "El usuario a crear quedaria de la siguiente manera:"
-                echo "$nameuser:x:$UID:$GID:$comentario:$home:$shell:"
-                echo "Desea continuar Y/n?"
-                read respuesta
-                if [[ $respuesta = Y || $respuesta = y ]]; then
-                    echo "Creando usuario"
-                    useradd -m -d $home -s $shell -c "$comentario" -u $UID -g $GID $nameuser
-                    elif [[ $respuesta = n || $respuesta = N ]]; then
-                    echo -e "Usuario no creado"
-                fi
-                
+                caso1
             ;;
             
             2)
                 echo "Crear un usuario sin Directorio Home, sin Shell, sin Grupo y un Comentario"
-                echo "insertar el usuario actual es $nameuser "
-                echo "insertar comentario: "
+                echo "El usuario actual es $nameuser "
+                echo "Insertar comentario: "
                 read comentario
                 useradd -M -N -r -s /bin/false -c "$comentario" $nameuser
             ;;
@@ -167,13 +210,19 @@ Opcion_Usuario (){
 #MENU PRINCIPAL
 menu (){
     echo -e "Sistema de administracion de usuarios y grupos \n¿Qué desea hacer?"
-    menu="Crear_Grupo Crear_Usuario Salir"
+    menu="Crear_Grupo Crear_Usuario Listar_Usuarios Listar_Grupos Salir"
     select menu in $menu
     do
         if [ $menu = "Crear_Grupo" ]; then
             Opcion_Grupo
             elif [ $menu = "Crear_Usuario" ]; then
             Opcion_Usuario
+            elif [ $menu = "Listar_Usuarios" ]; then
+            cat /etc/passwd | cut -d: -f1,3,5 | nl
+            menu
+            elif [ $menu = "Listar_Grupos" ]; then
+            cat /etc/group | cut -d: -f1,3,4 | nl
+            menu
             elif [ $menu = "Salir" ]; then
             echo "Fin de la sesion"
             exit
